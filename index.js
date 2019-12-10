9f6f0c7ed1dcb3e44ea0536f745e8391944459a3
Adam <awhiles2011@gmail.com>
2019-12-10 14:27:47 -0600

    fix font resources / add HomeScreen

--- index.js
+++ index.js
@@ -3,14 +3,14 @@
  */
 import * as React from 'react';
 import {AppRegistry} from 'react-native';
-import App from './App';
+import AppNavigator from './App';
 import {name as appName} from './app.json';
 import {Provider as PaperProvider} from 'react-native-paper';
 
 export default function Main() {
   return (
     <PaperProvider>
-      <App />
+      <AppNavigator />
     </PaperProvider>
   );
 }
